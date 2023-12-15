ba3bf364d1c04d8c4ee7a7108f5f0901730e688e
Sofi Bel <slipway.beds_0m@icloud.com>
2023-12-15 17:15:09 +0300

    fix 2 task for null

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,7 +10,7 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  return name.length > 3 && !name.includes(" ") && name !== "null";
+  return name !== null && name.length > 3 && !name.includes(" ");
 }
 
 function sayHello() {
